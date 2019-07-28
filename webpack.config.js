aac57e8b07c3ffa0426d983183f1db73ea9ecb4b
richifg <richifg@gmail.com>
2019-07-27 22:01:58 -0600

    changed build output folder to 'public'

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/js/app.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
   },
   module: {
